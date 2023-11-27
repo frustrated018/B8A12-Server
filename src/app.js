151c15cfca47b9559ec70ee947de8477c716b092
frustrated018 <niloyd018@gmail.com>
2023-11-27 12:19:09 +0600

    Added middlewares to the server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const applyMiddlewares = require("./middlewares/applyMiddlewares");
 require("dotenv").config;
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Applying middlewares
+applyMiddlewares(app);
 
 app.get("/health", (req, res) => {
   res.send("Tech trends is predicting trends....");
--- src/middlewares/applyMiddlewares.js
+++ src/middlewares/applyMiddlewares.js
@@ -1,17 +1,20 @@
 const cors = require('cors');
 const express = require("express");
+const { LOCAL_CLIENT, CLIENT } = require('../config/default');
 
 const applyMiddlewares=(app)=>{
     app.use(
         cors({
           origin: [
-            "http://localhost:5173",
+            LOCAL_CLIENT,
+            CLIENT
           ],
           credentials: true,
         })
       );
       app.use(express.json());
     //TODO: Not using cookie Parser right now because I don't know how I will send the JWT data
+    // if you use Cookins don't forget to install cookie parser
 }
 
 module.exports = applyMiddlewares
