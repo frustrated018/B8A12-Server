c418edc2e992e5559397d934123e6498e5423bfe
frustrated018 <niloyd018@gmail.com>
2023-11-28 20:45:07 +0600

    Added new routs to users api

--- src/Models/UserSchema.js
+++ src/Models/UserSchema.js
@@ -3,16 +3,22 @@ const { model, Schema } = require("mongoose");
 const UserSchema = new Schema({
   name: {
     type: String,
+    required: true,
   },
   image: {
     type: String,
   },
   email: {
     type: String,
+    required: true,
+    unique: true,
   },
- 
+  role: {
+    type: String,
+    default: "user",
+  },
 });
 
-const User = model("user", UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
--- src/routes/users/usersRoutes.js
+++ src/routes/users/usersRoutes.js
@@ -1,4 +1,5 @@
 const User = require("../../Models/UserSchema");
+const { ObjectId } = require("mongoose").Types;
 
 const router = require("express").Router();
 
@@ -8,22 +9,52 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/add", async (req, res) => {
-    try {
-      const newUser = req.body.userInfo;
-      const query = { email: newUser.email };
-      const existingUser = await User.findOne(query);
-  
-      if (existingUser) {
-        return res.status(400).send({ message: "User already exists", insertedId: null });
-      }
-  
-      const result = await User.create(newUser);
-      res.status(201).send({ message: "User created successfully", insertedId: 1 });
-    } catch (error) {
-      console.error("Error creating user:", error);
-      res.status(500).send({ message: "Internal server error", insertedId: null });
+  try {
+    const newUser = req.body.userInfo;
+    const query = { email: newUser.email };
+    const existingUser = await User.findOne(query);
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .send({ message: "User already exists", insertedId: null });
     }
-  });
-  
+
+    const result = await User.create(newUser);
+    res
+      .status(201)
+      .send({ message: "User created successfully", insertedId: 1 });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res
+      .status(500)
+      .send({ message: "Internal server error", insertedId: null });
+  }
+});
+
+// Delete User
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await User.deleteOne(query);
+    res.send(result);
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).send({ message: "Internal server error", deletedCount: 0 });
+  }
+});
+
+router.patch("/makeadmin/:id", async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updatedDoc = {
+    $set: {
+      role: "admin",
+    },
+  };
+  const result = await User.updateOne(filter, updatedDoc);
+  res.send(result);
+});
 
 module.exports = router;
