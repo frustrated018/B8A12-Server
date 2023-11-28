78e6bc06cf4a073863d3b7497629e3d69c619838
frustrated018 <niloyd018@gmail.com>
2023-11-29 00:23:32 +0600

    Updated the fake data and productsschema

--- src/Models/Product.js
+++ src/Models/Product.js
@@ -3,27 +3,39 @@ const { model, Schema } = require("mongoose");
 const ProductSchema = new Schema({
   name: {
     type: String,
+    required: true, // Make the name a required field
   },
   image: {
     type: String,
+    required: true,
   },
   tags: {
     type: [String],
+    required: true,
   },
   upvoteCount: {
     type: Number,
+    default: 0, 
   },
   downvoteCount: {
     type: Number,
+    default: 0, 
   },
   timestamp: {
-    type: String,
+    type: Date,
   },
   shortDescription: {
     type: String,
+    required: true,
   },
   longDescription: {
     type: String,
+    required: true,
+  },
+  productStatus: {
+    type: String,
+    enum: ["pending", "approved", "rejected"], // Use an enum for productStatus to limit possible values
+    default: "pending",
   },
 });
 
