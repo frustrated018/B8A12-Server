c052b49623d94cb5bd1eff90bbf4a6c932713dd9
frustrated018 <niloyd018@gmail.com>
2023-11-30 21:40:54 +0600

    Added firebase client links

--- src/config/default.js
+++ src/config/default.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const config = {
   LOCAL_CLIENT: process.env.LOCAL_CLIENT,
-  CLIENT: process.env.CLIENT,
+  CLIENT_ONE: process.env.CLIENT_ONE,
+  CLIENT_TWO: process.env.CLIENT_TWO,
 };
 
 module.exports = config;
--- src/middlewares/applyMiddlewares.js
+++ src/middlewares/applyMiddlewares.js
@@ -1,21 +1,14 @@
-const cors = require('cors');
+const cors = require("cors");
 const express = require("express");
-const { LOCAL_CLIENT, CLIENT } = require('../config/default');
+const { LOCAL_CLIENT, CLIENT_ONE, CLIENT_TWO } = require("../config/default");
 
-const applyMiddlewares=(app)=>{
-    app.use(
-        cors({
-          origin: [
-            LOCAL_CLIENT,
-            CLIENT
-          ],
-          // credentials: true, 
-          // TODO: Remove comment when you use jwt
-        })
-      );
-      app.use(express.json());
-    //TODO: Not using cookie Parser right now because I don't know how I will send the JWT data
-    // if you use Cookins don't forget to install cookie parser
-}
+const applyMiddlewares = (app) => {
+  app.use(
+    cors({
+      origin: [LOCAL_CLIENT, CLIENT_ONE, CLIENT_TWO],
+    })
+  );
+  app.use(express.json());
+};
 
-module.exports = applyMiddlewares
+module.exports = applyMiddlewares;
